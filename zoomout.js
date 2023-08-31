da2e7f86fc76f38b532af31dac2606fcb50acbe4
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-08-31 15:22:37 -0300

    incorporated darkie effect as zooming out

--- zoomout.js
+++ zoomout.js
@@ -2,16 +2,16 @@
 
    const zoomImage = document.getElementById("zoomImage");
 
+   const overlays2 = document.getElementsByClassName("overlay2");
+
    window.addEventListener("scroll", () => {
    
      // Calculate the scale based on scroll position
      console.log(window.scrollY);
+
      const scale = 1 + window.scrollY * -0.00012;
+
     
-   
-     /*console.log(window.scrollY);
-     console.log(scale);
-   */
      // Limit the minimum scale to 0.9
    
      const minScale = 0.94;
@@ -19,10 +19,20 @@
      const effectiveScale = Math.max(scale, minScale);
    
      console.log(effectiveScale);
+
+     const darkness = (1 - effectiveScale) * 10 ;
+
+     console.log(darkness);
+
+
+
      // Apply the scale transform to the image
    
     if(window.scrollY > 100){
      zoomImage.style.transform = `scale(${effectiveScale})`;
-     
-  } 
+     overlays2[0].style.opacity = darkness;
+     overlays2[1].style.opacity = darkness;
+     overlays2[2].style.opacity = darkness;
+     overlays2[3].style.opacity = darkness;
+}
    });
